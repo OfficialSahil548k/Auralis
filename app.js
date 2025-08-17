6e1fe53435d5e3d1701388f90e0106c63360a625
Sahil Sharma <officialsahil548k@gmail.com>
2025-08-17 16:58:58 +0530

    handling errors

--- app.js
+++ app.js
@@ -6,80 +6,100 @@ const listing = require('./models/listing.js');
 const path = require('path');
 const methodOverride = require('method-override');
 const engine = require('ejs-mate');
-
+const WrapAsync = require('./utils/WrapAsync.js');
+const ExpressError = require('./utils/ExpressError.js');
+const Joi = require('joi');
+const { listingSchema } = require('./Schema.js');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'views'));
-app.use(express.urlencoded({extended : true}));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(methodOverride('_method'));
-app.engine('ejs',engine);
-app.use(express.static(path.join(__dirname,'/public')));
+app.engine('ejs', engine);
+app.use(express.static(path.join(__dirname, '/public')));
 
 const MongoURL = 'mongodb://127.0.0.1:27017/Auralis';
+async function main() {
+    await mongoose.connect(MongoURL);
+}
 
-
-async function main(){
-   await mongoose.connect(MongoURL);
+const validateListing = (re,res,next) =>{
+    const data = req.body.listing;
+    let {error} = listingSchema.validate(data);
+    if (error) {
+        let errMsg = error.details.map((el) =>{
+            
+        })
+        throw new ExpressError(400, "Send Valid data of location");
+    }
 }
 
 main()
-    .then(()=>{
+    .then(() => {
         console.log("Connected to db");
-    }).catch((err)=>{
+    }).catch((err) => {
         console.log(err);
     });
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('listings/home.ejs');
 });
 
-app.get('/listing', async (req,res)=>{
+app.get('/listing', WrapAsync(async (req, res, next) => {
     let allListing = await listing.find({});
-    res.render('listings/index.ejs',{allListing})
-});
-
-app.get('/listing/new', async (req,res) =>{
-    res.render('listings/new.ejs');
-});
-
-app.get('/listing/:id', async (req,res) =>{
-    let {id} = req.params;
-    const list = await listing.findById(id);
-    res.render('listings/show.ejs',{list});
-});
-
-app.post('/listing', async(req,res)=>{
-    let { title, description, price, location, country} = await req.body;
-    await listing.insertOne({
-        title : title,
-        description : description,
-        price : price,
-        location : location,
-        country : country
-    });
-    res.redirect('/listing');
+    res.render('listings/index.ejs', { allListing })
 })
+);
 
-app.get('/listing/:id/edit', async (req,res) =>{
-    const {id} = req.params;
+app.get('/listing/new', WrapAsync(async (req, res) => {
+    res.render('listings/new.ejs');
+}));
+
+app.get('/listing/:id', WrapAsync(async (req, res) => {
+    let { id } = req.params;
     const list = await listing.findById(id);
-    res.render('listings/edit.ejs',{list});
-});
+    res.render('listings/show.ejs', { list });
+}));
 
-app.put('/listing/:id', async (req,res) =>{
-    const {id} = await req.params;
-    const {title, description, price, location, country} = await req.body;
-    await listing.findOneAndUpdate({_id : id}, {title : title, description : description, price : price, location : location, country : country});
+app.post('/listing',validateListing, WrapAsync(async (req, res, next) => {
+    const newListing = new listing(req.body.listing);
+    await newListing.save();
+    res.redirect('/listing');
+}));
+
+app.get('/listing/:id/edit', WrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const list = await listing.findById(id);
+    res.render('listings/edit.ejs', { list });
+}));
+
+app.put('/listing/:id',validateListing, WrapAsync(async (req, res) => {
+    const { id } = req.params;
+    if (!req.body || !req.body.listing) {
+        throw new ExpressError(400, "Send Valid data of location");
+    }
+    await listing.findOneAndUpdate({ _id: id }, req.body.listing);
     res.redirect(`/listing/${id}`);
-});
+}));
 
-app.delete('/listing/:id', async (req,res)=>{
-    const {id} = req.params;
-    await listing.deleteOne({_id : id});
+app.delete('/listing/:id', WrapAsync(async (req, res) => {
+    const { id } = req.params;
+    await listing.deleteOne({ _id: id });
     console.log("Successfully deleted");
     res.redirect('/listing');
-})
+}));
 
-app.listen(Port,  () =>{
+app.use((req, res, next) => {
+    next(new ExpressError(404, 'Page Not Found'));
+});
+
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Something went wrong!" } = err;
+    res.status(statusCode).render('listings/error.ejs', {message});
+    // res.status(statusCode).send(message);
+});
+
+app.listen(Port, () => {
     console.log(`app is running on http://localhost:${Port}/`);
 });
