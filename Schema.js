23782eb4d539c3801d95d4998430c9984129d189
Arjun Chandel <arjunchandel910@gmail.com>
2025-08-29 23:43:37 +0530

    fixed typo in Schema.js.

--- Schema.js
+++ Schema.js
@@ -14,9 +14,9 @@ module.exports.listingSchema = Joi.object({
   }).optional(),
 }).required();
 
-module.exports.reviewSchema = joi.object({
-    review: joi.object({
-        rating: joi.number().required().min(1).max(5),
-        comment: joi.string().allow("", null)
-    }).required(),
+module.exports.reviewSchema = Joi.object({
+  review: Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    comment: Joi.string().allow("", null),
+  }).required(),
 });
